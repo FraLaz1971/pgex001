de6df98e4aa7ea2237693d0c482d9bef5424d014
fralaz1971 <fralaz1971@gmail.com>
2025-12-10 16:50:19 +0100

    created app to extract submatrix (need to be tested more)

--- Makefile
+++ Makefile
@@ -11,7 +11,8 @@ FDFLAGS=-static -L$(PGPLOT_DIR) -Wl,-rpath=$(PGPLOT_DIR)
 LDFLAGS=$(FDFLAGS)LDFLAGS
 LIBS=-lpgplot -lX11 -lxcb -lXau -lXdmcp -lpng -lz
 SRCS=pgs.f p1_1.f p2.f pgs2096x2338.f pgsc2096x2338.f pgs4080x3072.f pgsc4080x3072.f pgs500x320.f \
-pgsc500x320.f rd.f raa.f waa.f wba.f m2pl.f test01.f test02.f pl2m.f ex1.f simple.f simple2.f
+pgsc500x320.f rd.f raasb.f waa.f wba.f m2pl.f test01.f test02.f pl2m.f ex1.f simple.f simple2.f \
+asubarr.f
 CSRCS=cpgsimple.c cpgarr2d.c
 OBJS = $(SRCS:.f=$(OEXT))
 COBJS = $(CSRCS:.c=$(OEXT))LDFLAGS
---
+++ asubarr.f
@@ -0,0 +1,131 @@
+C maximum dimension of the array is 5000x5000
+      PROGRAM TB
+	    IMPLICIT NONE
+        CHARACTER*80 IFNAM
+	    INTEGER FY,LY,FX,LX
+	    PRINT *,'ENTER INPUT FILENAME'
+	    READ *,IFNAM
+	    PRINT *,'ENTER FIRST X INDEX'
+	    READ *,FX
+	    PRINT *,'ENTER LAST X INDEX'
+	    READ *,LX
+	    PRINT *,'ENTER FIRST Y INDEX'
+	    READ *,FY
+	    PRINT *,'ENTER LAST Y INDEX'
+	    READ *,LY
+	    CALL M2SM(IFNAM,FX,LX,FY,LY)
+        STOP
+      END
+C READ ASCII 8 BITS ARRAY (MATRIX)
+C WITH FIRST Y AND LAST X
+C      FIRST X AND LAST X
+C AS INPUT
+C AND SAVE TO ASCII MATRIX FILE
+      SUBROUTINE M2SM(IFNAM,FX,LX,FY,LY)
+        IMPLICIT NONE
+	    INTEGER FY,LY,FX,LX,X,Y
+        CHARACTER*80 IFNAM,OFNAM
+        CHARACTER*1 RBYTE
+        CHARACTER*10 SUF1
+        INTEGER MXIDIM,MXJDIM,IVAL,I3VAL,POSI
+C Maximum expected dimension
+        PARAMETER(MXIDIM=5000,MXJDIM=5000)
+C MXIDIM is the maximum dimension on the Y axis (width)
+C MXJDIM is the maximum dimension on the X axis (height)
+        CHARACTER*(4*MXIDIM) LINE
+        CHARACTER*(4*MXIDIM) OLINE
+        CHARACTER*4 BNUM
+        INTEGER CN,CNT,CNT2,SP
+        LOGICAL DEBUG, F
+C WIDTH AND HEIGHT ARE DIMENSION OF THE SUBARRAY
+C TW IS THE TOTAL WIDTH OF THE INPUT MATRIX
+C TH IS THE TOTAL HEIGHT OF THE INPUT MATRIX
+        INTEGER I,J,WIDTH,HEIGHT,TW,TH
+        WIDTH=LX-FX+1
+        HEIGHT=LY-FY+1
+        DEBUG = .FALSE.
+C Create output file name, same basename, pre extension,.asc extension
+        WRITE(SUF1,'(BZ,I4,''x'',I4,''_'')')WIDTH,HEIGHT
+        DO 40,I=1,LEN(SUF1)
+          IF (SUF1(I:I).EQ.' ')SUF1(I:I)='_'
+40      CONTINUE
+        POSI=INDEX(IFNAM,'.')
+        OFNAM=IFNAM(1:POSI-1)//SUF1//'.asc'
+        OPEN(11,FILE=IFNAM,ERR=9000)
+        OPEN(12,FILE=OFNAM,ERR=9500)
+        F=.FALSE.
+        BNUM=' '
+        CN=1
+        CNT=0
+        TW=20000
+        TH=5000
+C POSITION OF THE OUTPUT SUBARR IS
+C X=MOD(CNT,WIDTH)
+C Y=CNT/WIDTH+1
+        DEBUG=.TRUE.
+        IF (DEBUG) PRINT *,'FX: ',FX,' LX: ',LX,' FY: ',FY,' LY: ',LY
+        DO 10,J=1,MXJDIM
+            SP=0
+            LINE=' '
+            READ(11,100,ERR=9100,END=30) LINE
+       IF((J.GE.FY).AND.(J.LE.LY)) THEN
+            DO 20,I=1,LEN(LINE)
+                RBYTE = LINE(I:I)
+                READ(RBYTE, '(I1)') IVAL
+                DEBUG=.FALSE.
+                IF(DEBUG)PRINT *,'I:',I,'RBYTE: ',RBYTE,' IVAL ',IVAL
+                DEBUG=.TRUE.
+                IF (RBYTE.NE.CHAR(32)) THEN
+                  BNUM(CN:CN) = RBYTE
+                  CN = CN + 1
+                ELSE IF ((CN.GT.1).AND.(RBYTE.EQ.CHAR(32)).AND.
+     & (BNUM(CN-1:CN-1).NE.CHAR(32)) ) THEN
+                    X=FX+MOD(CNT,TW)
+                    READ(BNUM, '(I3)', ERR=9300) I3VAL
+                    IF (DEBUG) PRINT *,'BNUM:',BNUM,' I3VAL',I3VAL
+                    IF (DEBUG) PRINT 200,(X-1)*4+1,(X-1)*4+4,(X-1)+1
+                     IF(X.LE.WIDTH) WRITE(OLINE((X-1)*4+1:(X-1)*4+4),
+     & '(I3,1X)',ERR=9400) I3VAL
+                    CNT = CNT + 1
+                    BNUM=' '
+                    CN=1
+                    SP=0
+                ELSE
+                    IF (DEBUG) PRINT *,'REST OF THE OPTIONS'
+                    IF (SP.GT.4) GOTO 15
+                    SP=SP+1
+                END IF
+20          CONTINUE
+15            PRINT *,'PROCESSED LINE',J
+              WRITE(12,'(A)',ERR=9400) OLINE(1:WIDTH*4)
+             END IF
+        IF (.NOT.F) THEN
+          TW=CNT
+          IF (DEBUG) PRINT *,'TOTAL WIDTH:',TW
+          F=.TRUE.
+        END IF
+10      CONTINUE
+C END OF ROWS TO PROCESS: GOTO 30
+30      CONTINUE
+        TH=J-1
+        DEBUG=.FALSE.
+        CLOSE(11)
+        CLOSE(112)
+        PRINT *,'PROCESSED ',CNT,' BYTES/ELEMENTS'
+        PRINT *,'TOTAL WIDTH',TW,'TOTAL HEIGHT',TH
+        PRINT *,'SUBARRAY WIDTH',WIDTH,'SUBARRAY HEIGHT',HEIGHT
+
+        GOTO 9999
+100     FORMAT(A)
+200     FORMAT('(X-1)*4+1',I4,'(X-1)*4+4',I4,'Y=',I4)
+9000    PRINT *,'ERROR IN OPENING INPUT FILE ',IFNAM
+        GOTO 9999
+9100    PRINT *,'ERROR IN READING THE ROW',J
+        GOTO 9999
+9300    PRINT *, 'ERROR CONVERTING NUMBER: ', BNUM
+        PRINT *, 'AT POSITION: ', I, J
+        GOTO 9999
+9400    PRINT *,'ERROR IN WRITING ASCII DATA'
+9500    PRINT *, 'ERROR OPENING OUTPUT FILE'
+9999    STOP
+      END
